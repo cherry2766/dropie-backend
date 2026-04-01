d9fa2a8c764368bf7f7b300f71bc1410d465b8c2
cherry <rlathrkd2766@gmail.com>
2026-04-01 18:52:21 +0900

    chore: initialize backend structure

---
+++ src/main/java/com/dropie/DropieApplication.java
@@ -0,0 +1,13 @@
+package com.dropie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dropie/DropieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
