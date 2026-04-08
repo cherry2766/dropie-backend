6717eccd99e8845e2d6eb07eab0b528131394960
cherry <rlathrkd2766@gmail.com>
2026-04-08 19:33:48 +0900

    refactor: DTO 리팩토링 (Request 생성자 추가, TokenResponse 제거)

--- src/main/java/com/dropie/dto/request/auth/LoginRequest.java
+++ src/main/java/com/dropie/dto/request/auth/LoginRequest.java
@@ -1,6 +1,8 @@
 package com.dropie.dto.request.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 // 클라이언트가 POST /auth/login 으로 보내는 JSON을 받는 객체
 // {
@@ -8,6 +10,8 @@ import lombok.Getter;
 //   "password": "1234"
 // }
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
     private String email;
